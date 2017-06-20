526a051b3c299665129e5c4df341852dc47925f3
Michael Prather <mprather1@gmail.com>
2017-06-20 18:57:22 +0000

    getone index.js

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ var request = require('request')
 var stream = require('stream')
 var http = require('http')
 var util = require('util')
+var url = require('url')
 var Transform = stream.Transform || require('readable-stream')
 
 http.createServer(function (req, res) {
+  var parsedURL = url.parse(req.url, true)
   var data = []
+  var parse = new ParseResponse()
 
-  if (req.method === 'GET') {
-    var parse = new ParseResponse()
-
+  if (req.method === 'GET' && !parsedURL.query.id) {
     parse.on('data', function (response) {
       data += response
     })
@@ -21,6 +22,20 @@ http.createServer(function (req, res) {
     })
 
     request('http://shintech.ninja:8000/get_all.php').pipe(parse)
+  }
+
+  if (req.method === 'GET' && parsedURL.query.id) {
+    parse.on('data', function (response) {
+      data += response
+    })
+
+    parse.on('end', function () {
+      res.setHeader('Content-Type', 'application/json')
+      res.write(data)
+      res.end()
+    })
+
+    request('http://shintech.ninja:8000/get_one.php?id=' + parsedURL.query.id).pipe(parse)
   }
 }).listen(3000)
 
