96700483e5ea3de4cb12c2d1035e1b5d229c56b8
Michael Prather <mprather1@gmail.com>
2017-06-20 02:12:18 +0000

    get_one

---
+++ get_one.php
@@ -0,0 +1,20 @@
+<?php
+  include 'database.php';
+  $id = null;
+  if(!empty($_GET['id'])) {
+    $id = $_REQUEST['id'];
+  }
+  
+  if (null==$id) {
+    echo 'Please include id in query...';
+  } else {
+    $pdo = Database::connect();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = 'SELECT * FROM models WHERE id = ?';
+    $q = $pdo->prepare($sql);
+    $q->execute(array($id));
+    $data = $q->fetch(PDO::FETCH_ASSOC);
+    echo json_encode($data);
+    Database::disconnect();
+  }
+?>
